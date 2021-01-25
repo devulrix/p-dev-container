796ed967627b327519aa7afe86f55dfd7e3b579a
devulrix <ulrich.hoelscher@outlook.com>
2021-01-25 10:28:35 +0100

    moving back to old helm approach

--- ci/scripts/copy-release.sh
+++ ci/scripts/copy-release.sh
@@ -16,10 +16,9 @@ echo "pivnet-cli"
 cp pivnet-release/pivnet-linux-amd64-$(cat pivnet-release/version) releases/pivnet
 
 echo "get helm"
-#curl -L0 https://get.helm.sh/helm-v"$(cat helm-release/version)"-linux-amd64.tar.gz --output helm.tar.gz
-#tar zxf helm.tar.gz 
-#cp linux-amd64/helm releases/helm
-cp helm-release/helm-v"$(cat helm-release/version)"-linux-amd64.tar.gz releases/helm
+curl -L0 https://get.helm.sh/helm-v"$(cat helm-release/version)"-linux-amd64.tar.gz --output helm.tar.gz
+tar zxf helm.tar.gz 
+cp linux-amd64/helm releases/helm
 
 echo "download latest stable kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
